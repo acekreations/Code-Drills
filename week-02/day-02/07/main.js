52474b8e1477b595876e73bb8f5e22e4fa18cf3f
Craig Melville <acekreations@gmail.com>
2018-08-01 20:03:19 -0700

    Day three complete

--- week-02/day-02/07/main.js
+++ week-02/day-02/07/main.js
@@ -1,22 +1,43 @@
 var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var operators = ["+", "-", "*", "/"];
 
+var userNums = [];
+var userOpertator;
+
 document.addEventListener('keydown', function(event){
   var key = event.key;
 
-  //Clear alert message
-  // document.getElementById('alert').innerHTML = "";
-
   if (nums.indexOf(key) >= 0) {
-    console.log("number: " + key);
+    userNums.push(key);
+    console.log("number: " + userNums);
+    document.getElementById("num1").innerHTML = key;
   }
   else if (operators.indexOf(key) >= 0) {
-    console.log("operator: " + key);
+    userOpertator = key;
+    console.log("operator: " + userOpertator);
   }
   else if (key === "=") {
     console.log("calculate");
+    if (userOpertator === "+") {
+        total = parseInt(userNums[0]) + parseInt(userNums[1]);
+        console.log(total);
+    }
+    if (userOpertator === "-") {
+        total = parseInt(userNums[0]) - parseInt(userNums[1]);
+        console.log(total);
+    }
+    if (userOpertator === "*") {
+        total = parseInt(userNums[0]) * parseInt(userNums[1]);
+        console.log(total);
+    }
+    if (userOpertator === "/") {
+        total = parseInt(userNums[0]) / parseInt(userNums[1]);
+        console.log(total);
+    }
   }
   else if (key === "c") {
-    console.log("");
+    console.log("Clear");
+    userNums = [];
+    userOpertator = "";
   }
 })
