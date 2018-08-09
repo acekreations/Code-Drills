642d943f7476fa94c424e18f9728f1cc2fb23d23
Craig Melville <acekreations@gmail.com>
2018-08-08 22:16:51 -0700

    complete week 3 day 3 exercise 5

--- week-03/day-02/05/challenge-prompt.js
+++ week-03/day-02/05/challenge-prompt.js
@@ -7,8 +7,36 @@
 function anagramCheck (str1, str2) {
 
   // ---------- Your Code Here ----------
+  //My way
+  // var str1 = str1.toLowerCase().replace(/ /g,'');
+  // var str2 = str2.toLowerCase().replace(/ /g,'');
+  //
+  // if (str1.length === str2.length) {
+  //   for (var i = 0; i < str1.length; i++) {
+  //     if (str2.indexOf(str1[i]) === -1) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+  // return false;
 
+  //Trying suggested way
+  var str1 = str1.toLowerCase().replace(/ /g,'');
+  var str2 = str2.toLowerCase().replace(/ /g,'');
 
+  if (str1.length === str2.length) {
+    for (var i = 0; i < str1.length; i++) {
+      str2 = str2.replace(str1[i], "");
+    }
+    if (str2.length <= 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  return false;
 
 
 
