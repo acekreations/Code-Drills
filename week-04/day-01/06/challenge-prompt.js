184d599999b0874a30489145b8855c0123912bd8
Craig Melville <acekreations@gmail.com>
2018-08-13 21:55:08 -0700

    complete week 4 day 1 exercise 6

--- week-04/day-01/06/challenge-prompt.js
+++ week-04/day-01/06/challenge-prompt.js
@@ -8,13 +8,30 @@ var database = firebase.database();
 $(function() {
 
   // -------------------- Your Code Here --------------------
+  database.ref().on("child_added", function(snapshot, key){
+    var newChat = $("<p>")
+    newChat.text(snapshot.val().chatLog);
+    $("#chat-display").append(newChat);
+  }, function(errorObject){
+    console.log(errorObject);
+  })
+
+  $("#chat-submit").on("click", function(){
+    event.preventDefault();
+    var chatInput = $("#chat-input").val();
+
+    database.ref().push({
+      chatLog: chatInput
+    });
+
+    $("#chat-input").val("");
+
+  })
 
 
-  
-  
- 
+
+
 
   // --------------------- End Code Area --------------------
 
 })
-
