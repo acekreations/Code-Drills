bd63c27fe3352cf1290d7aa52c56fad57a0a0bde
Craig Melville <acekreations@gmail.com>
2018-08-19 12:58:05 -0700

    week 4 day 5 excercise 4

--- week-04/day-05/04/challenge-prompt.js
+++ week-04/day-05/04/challenge-prompt.js
@@ -9,8 +9,21 @@
 // -------------------------------------------------------
 function fibonacci(num) {
   // -------------------- Your Code Here --------------------
+  var i = 0;
+  var fibonacci = [0, 1];
 
-
+  //num-2 accounts for starting at zero and for the extra loop that while takes before the statement is true
+  if (num === "0") {
+    return "Please enter a larger number";
+  }
+  else if (num === "1") {
+    return "0";
+  }
+  while (i < num-2) {
+    fibonacci.push(fibonacci[i] + fibonacci[i+1]);
+    i++;
+  }
+  return fibonacci;
 
 
 
@@ -23,9 +36,9 @@ function fibonacci(num) {
 $(function() {
   $(document).on("click", "button[type=submit]", function(event){
     event.preventDefault();
-    
+
     num = $("input").val().trim();
     $("#output-area").text(fibonacci(num));
 
   });
-})
+});
