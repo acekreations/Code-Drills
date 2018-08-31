5160714f7ad7b59148b41f24e7735a470b553982
Craig Melville <acekreations@gmail.com>
2018-08-30 20:04:18 -0700

    day three complete

--- week-06/day-03/05/challenge-prompt.js
+++ week-06/day-03/05/challenge-prompt.js
@@ -7,8 +7,17 @@ ignore values that aren't numbers
 Starter Code :*/
 function smallestNumber(arr) {
   // code starts here
+  var smallestNum;
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof smallestNum === "undefined" && typeof arr[i] === "number") {
+      smallestNum = arr[i];
+    }
+    else if (typeof arr[i] === "number" && arr[i] < smallestNum) {
+      smallestNum = arr[i];
+    }
+  }
 
-  
+  return smallestNum;
 
 
 
@@ -18,8 +27,6 @@ function smallestNumber(arr) {
 
 
 
-
-  
   // code ends here
 }
 var output = smallestNumber([4, 'lincoln', 9, 'octopus', 'a', 5, 10, 'hello']);
